445467a51a74b5ac2f38392e80f6acb4eb720fce
Wojciech Jureczka <wjureczka@gmail.com>
2020-06-21 20:03:22 +0200

    Clean up

--- APBD-KOLOKWIUM/Controllers/ArtistsController.cs
+++ APBD-KOLOKWIUM/Controllers/ArtistsController.cs
@@ -34,7 +34,7 @@ namespace APBD_KOLOKWIUM.Controllers
                 artist.Nickname,
                 events = artist.ArtistEvents
                 .OrderByDescending(artistEvent => artistEvent.Event.StartDate)
-                .Select(artistEvent => new { artistEvent.Event.IdEvent, artistEvent.Event.Name, artistEvent.Event.StartDate, artistEvent.Event.EndDate })
+                .Select(artistEvent => new { artistEvent.Event.IdEvent, artistEvent.Event.Name, artistEvent.Event.StartDate, artistEvent.Event.EndDate, artistEvent.PerformanceDate })
             }).ToListAsync();
             // https://github.com/dotnet/efcore/issues/19639
 
